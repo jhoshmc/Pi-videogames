62d389baacff3063197ff28a6b8b38f37b0a7077
caleb <jhoshmc201@gmail.com>
2024-01-24 19:41:25 -0500

    modificando la ruta videogames por game

--- api/src/routes/mainRouter.js
+++ api/src/routes/mainRouter.js
@@ -3,7 +3,13 @@ const { Router } = require("express");
 // Ejemplo: const authRouter = require('./auth.js');
 
 const getGernres = require("../handlres/genresHandler");
-const gameRouter = require("./gameRouter");
+const videogamesHandler = require("../handlres/videogamesHandler");
+const videogamesIdHandler = require("../handlres/videogamesIdHandler");
+const videogamesNameHandler = require("../handlres/videogamesNameHandler");
+const postVideogamesHandler = require("../handlres/postVideogamesHandler");
+const deleteVideogame = require("../handlres/deleteVideogame");
+
+// const gameRouter = require("./gameRouter");
 //const postRouter = require("./postRouter");
 
 const router = Router();
@@ -11,8 +17,23 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
-router.use("/videogames", gameRouter);
+// router.use("/videogames", gameRouter);
 //router.use("/videogames", postRouter);
+router.get("/game", videogamesHandler);
+
+// traer por name:
+
+router.get("/game/name", videogamesNameHandler);
+// traer por id:
+
+router.get("/game/:id", videogamesIdHandler);
+
+// post game:
+
+router.post("/game", postVideogamesHandler);
+
+//delete game:
+router.delete("/game/:id", deleteVideogame);
 router.get("/genres", getGernres);
 
 module.exports = router;
